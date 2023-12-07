2ef9e495a8aeb703bc0cc92327780e53af99d852
Rakai Seto <mamangrakai@gmail.com>
2023-12-08 00:15:54 +0700

    done demo

---
+++ resources/views/partial/header.blade.php
@@ -0,0 +1,14 @@
+<header class="dark-bb">
+    @if(request()->session()->get('token') !== null and request()->session()->get('token') !== '')
+    <nav class="navbar navbar-expand-lg px-3" style="justify-content: space-between">
+        <a href="/" class="text-decoration-none">
+            <h3>Mekaniko Presale</h3>
+        </a>
+        <a href="/signout" class="btn-2 text-decoration-none">Sign Out</a>
+        @else
+        <nav class="navbar navbar-expand-lg px-3 justify-content-center">
+            <h3>Mekaniko Presale</h3>
+        @endif
+
+    </nav>
+</header>
